64cb90bcd61cc0e1883b76585d58887f055d1bf3
Samuel Rundel <samuel.rundel@live.de>
2020-01-27 15:02:31 +0100

    DataManger-Class Initial Release

---
+++ VokabelCarsten/Classes/DataManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+//
+//Diese Klasse wird das bereitstellen, laden und verwalten von Tabellen übernehmen.
+//Der Konstructor wird beim Start eine Datei mit allen möglichen öffnen
+//
+
+namespace VokabelCarsten.Classes
+{
+    class DataManager
+    {
+        public static readonly DataManager _obj = new DataManager();
+
+        public static readonly string vocabBoxList = "/data/data/com.DHBW_SoftEng.vokabelcarsten/vocabBoxes.csv";
+        public void Display()
+        {
+            Console.WriteLine(true);
+        }
+        DataManager() { }
+
+        //Read File to get list of all VocabBoxes
+
+    }
+}
