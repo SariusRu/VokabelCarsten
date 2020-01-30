3ea98f04b6533967ad69b62a1bbf8e93529a56a2
Bettina <bettina.kimpfler99@web.de>
2020-01-30 09:01:07 +0100

    first implementation of class vocab

---
+++ VokabelCarsten/Classes/Vocabs.cs
@@ -0,0 +1,51 @@
+﻿//Bettina 
+
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace VokabelCarsten.Classes
+{
+    class Vocab
+    {
+        public string side1 { get; set; }       //Vocab on side 1 e.g. "to walk"
+        public string side2 { get; set; }       // Vocab on side 2 e.g."laufen"
+        public int level { get; set; } = 0;
+        public int id { get; set; }
+
+        public Vocab(string si1, string si2, int ID) 
+        {
+            side1 = si1;
+            side2 = si2;
+            id = ID;
+        }
+
+        public void EditVocab(string si1, string si2, int lev)
+        {
+            side1 = si1;
+            side2 = si2;
+            level = lev;
+        }
+        
+
+        public void increaseLevel()
+        {
+            if (level == 0)
+            { }
+            else { level--; }
+        }
+
+        public void decreaseLevel()
+        {
+            if (level == 6) { }
+            else { level++; }
+
+        }
+    }
+}
