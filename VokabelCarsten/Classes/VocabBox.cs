693efe93a41f6d4984f2cf183139bb25ea458a69
noah <noahkeltsch@gmx.de>
2020-02-17 12:07:33 +0100

    Update to VocabBoxClass

--- VokabelCarsten/Classes/VocabBox.cs
+++ VokabelCarsten/Classes/VocabBox.cs
@@ -7,7 +7,7 @@ namespace VokabelCarsten
 	//noah
 	class VocabBox
 	{
-
+	private int idcounter { get; set; }
 	public int id { get; set; }
 	public string name { get; set; }
 	public string spalte1 { get; set; }
@@ -19,15 +19,18 @@ namespace VokabelCarsten
 
 		public VocabBox(string Na, string S1, string S2)
         {
+			idcounter = 0;
 			name = Na;
 			spalte1 = S1;
 			spalte2 = S2;
 			Vokabeln = new List<Vocab>();
 			rndGenerator = new Random();
 		}
-		public void addVokabel(Vocab Vokabel)
+		public void addVokabel(string si1, string si2)
         {
-			Vokabeln.Add(Vokabel);
+			idcounter++;
+			Vocab voc = new Vocab(si1, si2, idcounter);
+			Vokabeln.Add(voc);
 		}
 		public void removeVokabel(int id)
         {
