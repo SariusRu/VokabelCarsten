8d98a61cc843a6134c2d4b2bd92c61d8dfd1b8df
DaniForUplay <daniel.schoellhorn@outlook.de>
2020-02-17 14:59:23 +0100

    Merged changes in Control class Alex and Daniel made into new branch.

--- VokabelCarsten/Classes/Control.cs
+++ VokabelCarsten/Classes/Control.cs
@@ -35,6 +35,10 @@ namespace VokabelCarsten.Classes
         public Control(string pGUI)
         {
             theGUI = pGUI;
+            /*vocabboxList.Add(new VocabBox("Eng-De", "englisch", "deutsch"));
+            vocabboxList.Add(new VocabBox("Ft-De", "französisch", "deutsch"));
+            vocabboxList.Add(new VocabBox("Sp-De", "spansich", "deutsch")); 
+            Dummy data for testing purposes*/
         }
         #endregion
 
@@ -133,25 +137,58 @@ namespace VokabelCarsten.Classes
 
         //How to identify Kasten? By Name, ID, something different?
 
+        public void printAllBoxes()
+        {
+            /*theGUI.appendTB_outputText("List of saved Vocab Boxes
+            for (int i = 0; i < vocabboxList.Count; i)
+            {
+                theGUI.appendTB_outputText(vocabboxList[i].name);
+            }*/
+        }
+
         public void createBox(string pName, string pColumn1, string pColumn2)
         {
-            //kasten.VocabBox(pName, pColumn1, pColumn2);
+            vocabboxList.Add(new VocabBox(pName, pColumn1, pColumn2));
+            //theGUI.appendTB_outputText("New Vocab Box " + pName + " added.");
+            vocabboxList.Add(new VocabBox("test", "to find items", "in between beginning and end"));     
         }
 
         public void deleteBox(int pID)
         {
-
+            int index = vocabboxList.IndexOf(vocabboxList.Find(item => item.name == pName));
+            if (index == -1)
+            {
+                //theGUI.appendTB_outputText("Vocab Box " + pName + " not found.");
+                return;
+            }
+            vocabboxList.RemoveAt(index); //If we choose to identify by ID and ID is the list index 
+            //IF ID is equal to the index, the IDs of all items have to be adjusted
+            //theGUI.appendTB_outputText("Vocab Box " + pName + " deleted.");
         }
 
         public void renameBoxName(string pName, string pNameNew)
         {
-            //kasten.setName(pNameNew);
+            int index = vocabboxList.IndexOf(vocabboxList.Find(item => item.name == pName));
+            if (index == -1)
+            {
+                //theGUI.appendTB_outputText("Vocab Box " + pName + " not found.");
+                return;
+            }
+            vocabboxList[index].name = pNameNew;
+            //theGUI.appendTB_outputText("Vocab Box name " + pName + " changed into " + pNameNew + ".");       
         }
 
         public void renameBoxColumns(string pName, string pColumn1, string pColumn2)
         {
-            //kasten.setColumn1(pColumn1);
-            //kasten.setColumn2(pColumn2);
+            int index = vocabboxList.IndexOf(vocabboxList.Find(item => item.name == pName));
+            if (index == -1)
+            {
+                //theGUI.appendTB_outputText("Vocab Box " + pName + " not found.");
+                return;
+            }
+            vocabboxList[index].spalte1 = pColumn1;
+            vocabboxList[index].spalte2 = pColumn2;
+            //theGUI.appendTB_outputText("Vocab Box " + pName + ": columns changed into " + pColumn1 + " and " + pColumn2 + ".");        
         }
 
         #endregion
@@ -162,7 +199,11 @@ namespace VokabelCarsten.Classes
 
         public void createVocab(string pSide1, string pSide2, int pBox)
         {
-            //vokabel.Vokabel(pSide1, pSide2, pBox);
+            /*How to proceed with Vocabs?
+             Where/how are Vocabs generated? (VocabBox takes an existing Vocab and adds it to its list of Vocabularies,
+             but giving VocabBox parameters and letting VocabBox generate the Vocab might be easier(?))
+             If that is decided: How to find a specific Vocab inside the Vocab lsit?
+             Searching bei Side 1 and Side 2 or by ID/name/whatever?*/
         }
 
         public void rmVocab(int pID)
