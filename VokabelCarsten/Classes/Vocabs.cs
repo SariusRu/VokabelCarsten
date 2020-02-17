b69441c87754f4de2b0a00ba79d519edb25ad2f7
Bettina <bettina.kimpfler99@web.de>
2020-02-17 11:06:43 +0100

    Updated Vocabs

--- VokabelCarsten/Classes/Vocabs.cs
+++ VokabelCarsten/Classes/Vocabs.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using System.Text;
 
@@ -9,16 +10,46 @@ using Android.Views;
 using Android.Widget;
 
 // von betti
+/**
+ * @file Vocabs.cs
+ * @author Bettina Kimpfler
+ * @date 30.01.2020
+ * @brief Vocab Class for Vocabulary Elemements
+ */
+
+
 
 namespace VokabelCarsten
 {
-    class Vocab
+    [Serializable]
+    public class Vocab
     {
+        /** \brief First side of Vocabulary */
         public string side1 { get; set; }       //Vocab on side 1 e.g. "to walk2
+
+        /** \brief Second side of Vocabulary */
         public string side2 { get; set; }       // Vocab on side 2 e.g."laufen"
+
+        /** \brief Level of Vocabulary */
         public int level { get; set; } = 0;
+
+        /** \brief ID of Vocabulary */
         public int id { get; set; }
 
+        public Vocab()
+        {
+
+        }
+
+        /**
+          * @brief Constructor of Vocab Element
+          *
+          *
+          * @param	si1  Side One of Vocab
+          * @param	si2  Side two of Vocab
+          * @param  ID   Id of Vocab
+          *
+          */
         public Vocab(string si1, string si2, int ID)   //Konstruktor
         {
             side1 = si1;
@@ -26,31 +57,49 @@ namespace VokabelCarsten
             id = ID;
         }
 
-
-        public void EditVocab(string si1, string si2, int lev)
+        /**
+          * @brief Function to Edit a Vocabulary
+          * @param	si1  New value of side one
+          * @param	si2  New value of side two
+          * @param  lev  New value of Level
+          *
+          */
+        public void editVocab(string si1, string si2, int lev)
         {
             side1 = si1;
             side2 = si2;
             level = lev;
         }
-
-        public void SetLevel(int lev)
-        {
-            level = lev;
-        }
-
-        public int GetLevel()
+        /**
+         * @brief Function to get the Level of a Vocab
+         * @return Level of the vocab
+         */
+        public int getLevel()
         {
             return level;
         }
 
+        /**
+         * @brief Function to set the level of a vocab
+         * @param lev  new Level of the vocab
+         */
+        public void setLevel(int lev)
+        {
+            level = lev;
+        }
+
+        /**
+         * @brief Function to increase the Level of the Vocab
+         */
         public void increaseLevel()
         {
             if (level == 0)
             { }
             else { level--; }
         }
-
+        /**
+         * @brief Function to decrease the Level of the Vocab
+         */
         public void decreaseLevel()
         {
             if (level == 6) { }
