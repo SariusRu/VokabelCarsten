62c2e8f2f599bfadd370787d9001aae48de51ccb
Samuel Rundel <samuel.rundel@live.de>
2020-02-25 13:51:27 +0100

    Test-Tests

--- VokabelCarsten.Test/UnitTest1.cs
+++ VokabelCarsten.Test/UnitTest1.cs
@@ -8,6 +8,13 @@ namespace VokabelCarsten.Test
         [TestMethod]
         public void TestMethod1()
         {
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(1, 0);
         }
     }
 }
