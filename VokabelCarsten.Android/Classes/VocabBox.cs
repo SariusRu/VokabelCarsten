1f41331a36c8ff3b57015a04dd45ed86ef05336b
Samuel Rundel <samuel.rundel@live.de>
2020-02-27 22:33:49 +0100

    Update VocabBox

--- VokabelCarsten.Android/Classes/VocabBox.cs
+++ VokabelCarsten.Android/Classes/VocabBox.cs
@@ -94,13 +94,13 @@ namespace VokabelCarsten
 		/// Method is not necessary until later versions
 		/// </summary>
 		/// <param name="id"<>/param>
-		/*public Vocab getVokabel(int id)
+		public Vocab getVokabel(int id)
         {
 
 				return Vokabeln[id];
 
 			
-        }*/
+        }
 
 		/// <summary>
 		/// Get random Vocab.
@@ -169,6 +169,11 @@ namespace VokabelCarsten
 		public void unloadVocabs()
 		{
 			Vokabeln.Clear();
+		}
+
+		public string getName()
+		{
+			return name;
 		}
 	}
 }
