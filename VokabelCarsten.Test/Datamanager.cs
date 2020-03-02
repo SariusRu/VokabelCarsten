2e7678e1d270fef6993602a7f6885af571728afb
Samuel Rundel <samuel.rundel@live.de>
2020-03-02 18:07:20 +0100

    Readding Tests, Actions

---
+++ VokabelCarsten.Test/Datamanager.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace VocabelCarsten.Test
+{
+    [TestClass]
+    public class DataManagerTest : VokabelCarsten.DataManager
+    {
+
+        private VokabelCarsten.VocabBox testBox = new VokabelCarsten.VocabBox("Test", "Englisch", "Deutsch", "Test.xml");
+        private VokabelCarsten.VocabBox testBox2 = new VokabelCarsten.VocabBox("Test2", "Englisch2", "Deutsch2", "Test2.xml");
+        public DataManagerTest()
+        {
+            testBox.addVokabel("Hallo", "Hello");
+            testBox.addVokabel("Tschüss", "Bye");
+            testBox2.addVokabel("Hallo", "Hello");
+            testBox2.addVokabel("Tschüss", "Bye");
+        }
+
+
+        [TestMethod]
+        public void SerializeTestVocabBox()
+        {
+            string result = xmlSerializer(ref testBox);
+            Assert.AreEqual("<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<VocabBox xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">\r\n  <id>0</id>\r\n  <name>Test</name>\r\n  <spalte1>Englisch</spalte1>\r\n  <spalte2>Deutsch</spalte2>\r\n  <Vokabeln>\r\n    <Vocab>\r\n      <side1>Hallo</side1>\r\n      <side2>Hello</side2>\r\n      <level>0</level>\r\n      <id>0</id>\r\n    </Vocab>\r\n    <Vocab>\r\n      <side1>Tschüss</side1>\r\n      <side2>Bye</side2>\r\n      <level>0</level>\r\n      <id>1</id>\r\n    </Vocab>\r\n  </Vokabeln>\r\n  <filePath>Test.xml</filePath>\r\n</VocabBox>", result);
+        }
+
+        [TestMethod]
+        public void SerializeTestVocabBoxList()
+        {
+            List<VokabelCarsten.VocabBox> testList = new List<VokabelCarsten.VocabBox>();
+            testList.Add(testBox);
+            testList.Add(testBox2);
+            string result = xmlSerializer(ref testList);
+            Assert.AreEqual("<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<ArrayOfVocabBox xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">\r\n  <VocabBox>\r\n    <id>0</id>\r\n    <name>Test</name>\r\n    <spalte1>Englisch</spalte1>\r\n    <spalte2>Deutsch</spalte2>\r\n    <Vokabeln>\r\n      <Vocab>\r\n        <side1>Hallo</side1>\r\n        <side2>Hello</side2>\r\n        <level>0</level>\r\n        <id>0</id>\r\n      </Vocab>\r\n      <Vocab>\r\n        <side1>Tschüss</side1>\r\n        <side2>Bye</side2>\r\n        <level>0</level>\r\n        <id>1</id>\r\n      </Vocab>\r\n    </Vokabeln>\r\n    <filePath>Test.xml</filePath>\r\n  </VocabBox>\r\n  <VocabBox>\r\n    <id>0</id>\r\n    <name>Test2</name>\r\n    <spalte1>Englisch2</spalte1>\r\n    <spalte2>Deutsch2</spalte2>\r\n    <Vokabeln>\r\n      <Vocab>\r\n        <side1>Hallo</side1>\r\n        <side2>Hello</side2>\r\n        <level>0</level>\r\n        <id>0</id>\r\n      </Vocab>\r\n      <Vocab>\r\n        <side1>Tschüss</side1>\r\n        <side2>Bye</side2>\r\n        <level>0</level>\r\n        <id>1</id>\r\n      </Vocab>\r\n    </Vokabeln>\r\n    <filePath>Test2.xml</filePath>\r\n  </VocabBox>\r\n</ArrayOfVocabBox>", result);
+        }
+
+        [TestMethod]
+        public void DeserializeTestVocabBox()
+        {
+            VokabelCarsten.VocabBox result = xmlDeserialize(ref testBox, "<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<VocabBox xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">\r\n  <id>0</id>\r\n  <name>Test</name>\r\n  <spalte1>Englisch</spalte1>\r\n  <spalte2>Deutsch</spalte2>\r\n  <Vokabeln>\r\n    <Vocab>\r\n      <side1>Hallo</side1>\r\n      <side2>Hello</side2>\r\n      <level>0</level>\r\n      <id>0</id>\r\n    </Vocab>\r\n    <Vocab>\r\n      <side1>Tschüss</side1>\r\n      <side2>Bye</side2>\r\n      <level>0</level>\r\n      <id>1</id>\r\n    </Vocab>\r\n  </Vokabeln>\r\n  <filePath>Test.xml</filePath>\r\n</VocabBox>");
+            Assert.AreEqual(testBox, result);
+        }
+
+        [TestMethod]
+        public void DeserializeTestVocabBoxList()
+        {
+            List<VokabelCarsten.VocabBox> testList = new List<VokabelCarsten.VocabBox>();
+            testList.Add(testBox);
+            testList.Add(testBox2);
+            List<VokabelCarsten.VocabBox> result = xmlDeserialize(ref testList, "<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<ArrayOfVocabBox xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">\r\n  <VocabBox>\r\n    <id>0</id>\r\n    <name>Test</name>\r\n    <spalte1>Englisch</spalte1>\r\n    <spalte2>Deutsch</spalte2>\r\n    <Vokabeln>\r\n      <Vocab>\r\n        <side1>Hallo</side1>\r\n        <side2>Hello</side2>\r\n        <level>0</level>\r\n        <id>0</id>\r\n      </Vocab>\r\n      <Vocab>\r\n        <side1>Tschüss</side1>\r\n        <side2>Bye</side2>\r\n        <level>0</level>\r\n        <id>1</id>\r\n      </Vocab>\r\n    </Vokabeln>\r\n    <filePath>Test.xml</filePath>\r\n  </VocabBox>\r\n  <VocabBox>\r\n    <id>0</id>\r\n    <name>Test2</name>\r\n    <spalte1>Englisch2</spalte1>\r\n    <spalte2>Deutsch2</spalte2>\r\n    <Vokabeln>\r\n      <Vocab>\r\n        <side1>Hallo</side1>\r\n        <side2>Hello</side2>\r\n        <level>0</level>\r\n        <id>0</id>\r\n      </Vocab>\r\n      <Vocab>\r\n        <side1>Tschüss</side1>\r\n        <side2>Bye</side2>\r\n        <level>0</level>\r\n        <id>1</id>\r\n      </Vocab>\r\n    </Vokabeln>\r\n    <filePath>Test2.xml</filePath>\r\n  </VocabBox>\r\n</ArrayOfVocabBox>");
+            Assert.AreEqual(testList, result);
+        }
+
+
+        [TestMethod]
+        public void getVocabBoxListTest()
+        {
+            List<VokabelCarsten.VocabBox> testList = VokabelCarsten.DataManager.staticDataManager.getVocabBoxList();
+            List<VokabelCarsten.VocabBox> testList2 = new List<VokabelCarsten.VocabBox>();
+            CollectionAssert.AreEqual(testList, testList2);
+            
+        }
+
+        [TestMethod]
+        public void getVocabBoxListAsArrayTest()
+        {
+            VokabelCarsten.VocabBox[] testArray = VokabelCarsten.DataManager.staticDataManager.getVocabBoxArray();
+            VokabelCarsten.VocabBox[] testArray2 = new VokabelCarsten.VocabBox[0];
+            CollectionAssert.AreEqual(testArray2, testArray);
+        }
+
+        [TestMethod]
+        public void selectVocabBoxTest()
+        {
+            staticDataManager.removeAllVocabBoxes();
+            staticDataManager.CreateVocabBox(testBox);
+            staticDataManager.CreateVocabBox(testBox2);
+            staticDataManager.selectVocabBox(0);
+            Assert.AreEqual(testBox.getName(), staticDataManager.loadedBox.getName());
+            staticDataManager.selectVocabBox(1);
+            Assert.AreEqual(testBox2.getName(), staticDataManager.loadedBox.getName());
+        }
+
+        [TestMethod]
+        public void removeVocabBoxTest()
+        {
+            staticDataManager.removeAllVocabBoxes();
+            staticDataManager.CreateVocabBox(testBox);
+            staticDataManager.CreateVocabBox(testBox2);
+            staticDataManager.deleteVocabBox(0);
+            staticDataManager.deleteVocabBox(0);
+            List<VokabelCarsten.VocabBox> testList2 = new List<VokabelCarsten.VocabBox>();
+            CollectionAssert.AreEqual(testList2, staticDataManager.getVocabBoxList());
+        }
+
+        [TestMethod]
+        public void restoreLoadedBoxTest()
+        {
+            staticDataManager.removeAllVocabBoxes();
+            staticDataManager.CreateVocabBox(testBox);
+            staticDataManager.CreateVocabBox(testBox2);
+            staticDataManager.selectVocabBox(0);
+            staticDataManager.restoreLoadedBox();
+            Assert.IsNull(staticDataManager.loadedBox);
+        }
+    }
+}
---
+++ VokabelCarsten.Test/Vocab.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VocabelCarsten.Test
+{
+    [TestClass]
+    public class VocabTest
+    {
+        [TestMethod]
+        public void returnSide1Checks()
+        {
+            //initialize
+            VokabelCarsten.Vocab testVocab = new VokabelCarsten.Vocab("Hallo", "Hello", 0);
+            //alter name
+            Assert.AreEqual("Hallo", testVocab.getSide1(), "The value wasn't returned correctly");
+
+        }
+
+        [TestMethod]
+        public void returnSide2Checks()
+        {
+            //initialize
+            VokabelCarsten.Vocab testVocab = new VokabelCarsten.Vocab("Hallo", "Hello", 0);
+            //alter name
+            Assert.AreEqual("Hello", testVocab.getSide2(), "The value wasn't returned correctly");
+
+        }
+
+        [TestMethod]
+        public void changeTranslation()
+        {
+            //initialize
+            VokabelCarsten.Vocab testVocab = new VokabelCarsten.Vocab("Hallo", "Hello", 0);            
+            //alter side1
+            testVocab.EditVocab("Tschüss", "Hello", 0);
+            //check results
+            Assert.AreEqual("Tschüss", testVocab.getSide1(), "The value wasn't changed correctly");
+            Assert.AreEqual("Hello", testVocab.getSide2(), "The value was changed even if it wasn't supposed to be chanegd.");
+
+            //alter side2
+            testVocab.EditVocab("Tschüss", "Bye", 0);
+            //Check Results
+            Assert.AreEqual("Tschüss", testVocab.getSide1(), "The value was changed even if it wasn't supposed to be chanegd.");
+            Assert.AreEqual("Bye", testVocab.getSide2(), "The value wasn't changed correctly");
+
+            //alter both sides
+            testVocab.EditVocab("Hallo", "Hello", 0);
+            //Check Results
+            Assert.AreEqual("Hallo", testVocab.getSide1(), "The value wasn't changed correctly");
+            Assert.AreEqual("Hello", testVocab.getSide2(), "The value wasn't changed correctly");
+
+        }
+
+        [TestMethod]
+        public void getLevelTest()
+        {
+            //initialize
+            VokabelCarsten.Vocab testVocab1 = new VokabelCarsten.Vocab("Hallo", "Hello", 0);
+            Assert.AreEqual(0, testVocab1.GetLevel(), "Test Vocab was initalized properly");
+            testVocab1.increaseLevel();
+            VokabelCarsten.Vocab testVocab2 = new VokabelCarsten.Vocab("Hallo", "Hello", 0);
+            //alter name
+            Assert.AreEqual(1, testVocab1.GetLevel(), "Level wasn't returned Properly");
+            Assert.AreEqual(0, testVocab2.GetLevel(), "Test Vocab was initalized properly");
+
+        }
+
+        [TestMethod]
+        public void increaseLevelTest()
+        {
+            VokabelCarsten.Vocab testVocab1 = new VokabelCarsten.Vocab("Hallo", "Hello", 0);
+            Assert.AreEqual(0, testVocab1.GetLevel(), "Test Vocab was initalized properly");
+            testVocab1.increaseLevel();
+            Assert.AreEqual(1, testVocab1.GetLevel(), "Level wasn't increased Properly");
+            testVocab1.increaseLevel();
+            testVocab1.increaseLevel();
+            testVocab1.increaseLevel();
+            testVocab1.increaseLevel();
+            testVocab1.increaseLevel();
+            Assert.AreEqual(6, testVocab1.GetLevel(), "Level wasn't increased Properly");
+            testVocab1.increaseLevel();
+            Assert.AreEqual(6, testVocab1.GetLevel(), "Level wasn't increased Properly");
+        }
+
+        [TestMethod]
+        public void decreaseLevelTest()
+        {
+            VokabelCarsten.Vocab testVocab1 = new VokabelCarsten.Vocab("Hallo", "Hello", 0);
+            Assert.AreEqual(0, testVocab1.GetLevel(), "Test Vocab was initalized properly");
+            testVocab1.decreaseLevel();
+            Assert.AreEqual(0, testVocab1.GetLevel(), "Level wasn't increased Properly");
+            testVocab1.increaseLevel();
+            testVocab1.increaseLevel();
+            testVocab1.increaseLevel();
+            Assert.AreEqual(3, testVocab1.GetLevel(), "Level wasn't increased Properly");
+            testVocab1.decreaseLevel();
+            Assert.AreEqual(2, testVocab1.GetLevel(), "Level wasn't increased Properly");
+            
+        }
+    }
+}
